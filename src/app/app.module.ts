1e5fd487080029b89b4b87cf1aece7b42e7aa9a0
singhpriansh <priyanshu.h786@gmail.com>
2021-10-06 18:29:47 +0530

    header footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,20 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { AngularMaterialModule } from './angular-material';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    AngularMaterialModule,
     BrowserAnimationsModule
   ],
   providers: [],
